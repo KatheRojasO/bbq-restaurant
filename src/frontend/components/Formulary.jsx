3e17f15cc4438bc7b51b47f6209055c8d8f2d93f
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-03-02 14:03:06 +0100

    Add form to add menu items

---
+++ src/frontend/components/Formulary.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+export default function Formulary() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [servedWith, setServedWith] = useState("");
+  const [price, setPrice] = useState("");
+  const [imageURL, setImageURL] = useState(""); 
+  const [category, setCategory] = useState("");
+
+  
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log("item","name:" + name, "descr:" + description, "pice:" + price, "served:" + servedWith, "img:" +imageURL, "cate:" + category);
+  }
+
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+      <h3>Add a new plate to Hot Grill page:</h3>
+      <label>
+        Product name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
+      <label>
+        Product description:
+        <input
+          type="text"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </label>
+      <label>
+        Served with:
+        <input
+          type="text"
+          value={servedWith}
+          onChange={(event) => setServedWith(event.target.value)}
+        />
+      </label>
+      <label>
+        Price:
+        <input
+          type="number"
+          value={price}
+          min="1"
+          onChange={(event) => setPrice(event.target.value)}
+        />
+      </label>
+      <label>
+        Image link:
+        <input
+          type="text"
+          value={imageURL}
+          onChange={(event) => setImageURL(event.target.value)}
+        />
+      </label>
+      <label>
+        Category:
+        <input
+          type="text"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
+      </label>
+      
+      <button>Add item</button>
+    </form>
+  );
+}
--- src/frontend/pages/MenuPage.jsx
+++ src/frontend/pages/MenuPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Formulary from "../components/Formulary";
 import ItemMenu from "../components/ItemMenu";
 
 export default function MenuPage({ data }) {
@@ -6,6 +7,7 @@ export default function MenuPage({ data }) {
   return (
     <div>
       <h2> Items Created: </h2>
+      <Formulary />
       {Items}
     </div>
   );
