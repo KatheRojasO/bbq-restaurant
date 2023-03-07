b1f224f65e3e3fa8d07aef36fd31a8f7f6787f42
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-03-07 15:48:53 +0100

    Style hero section for every device

---
+++ src/frontend/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="hero-items">
+          <h1>HOT GRILL</h1>
+          <h2>Your BBQ. How you want it.</h2>
+          <button>Check our menu</button>
+        </div>
+      </div>
+    </div>
+  );
+}
