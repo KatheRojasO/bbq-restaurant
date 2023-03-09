199ff7fca8ebd4ecfa01296361f51cfcdb49e9af
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-03-09 18:18:44 +0100

    Fix css in forms

--- src/frontend/components/ContactForm.jsx
+++ src/frontend/components/ContactForm.jsx
@@ -2,40 +2,54 @@ import React, { useState } from "react";
 // import emailjs from "emailjs-com";
 
 export default function Form() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [date, setDate] = useState("");
-//   const [time, setTime] = useState("");
+  //   const [name, setName] = useState("");
+  //   const [email, setEmail] = useState("");
+  //   const [date, setDate] = useState("");
+  //   const [time, setTime] = useState("");
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
 
-//     const bookingObject = {
-//       name: name,
-//       email: email,
-//       date: date,
-//       time: time
-//     };
+  //     const bookingObject = {
+  //       name: name,
+  //       email: email,
+  //       date: date,
+  //       time: time
+  //     };
 
-//     emailjs.sendForm(
-//       process.env.REACT_APP_EMAILJS_SERVICE_ID,
-//       process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-//       bookingObject.current,
-//       process.env.REACT_APP_EMAILJS_USER_ID
-//     );
-//     event.target.reset();
-//   };
+  //     emailjs.sendForm(
+  //       process.env.REACT_APP_EMAILJS_SERVICE_ID,
+  //       process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  //       bookingObject.current,
+  //       process.env.REACT_APP_EMAILJS_USER_ID
+  //     );
+  //     event.target.reset();
+  //   };
 
   return (
-    <div id="form">
-      <form className="form-info">
-        <input type="text" placeholder="Full name"/>
-        <input type="text" placeholder="Email"/>
-        <input type="date" placeholder="Day"/>
-        <input type="time" placeholder="Time"/>
-        <button>Book a table</button>
-      </form>
-    </div>
+    <form className="contact-form">
+      <div className="form-inputs">
+        <label className="add-form-label">
+          Full name:
+          <input type="text" />
+        </label>
+        <label className="add-form-label">
+          Email:
+          <input type="email" />
+        </label>
+        <label className="add-form-label">
+          Date:
+          <input type="date" min="2023-01" />
+        </label>
+        <label className="add-form-label">
+          Time:
+          <input type="time"  min="11:00" max="21:00" />
+        </label>
+      </div>
+      <div className="form-button">
+        <button className="book-button">Book a table</button>
+      </div>
+    </form>
   );
 }
 
@@ -46,3 +60,4 @@ export default function Form() {
 //         <input type="time" placeholder="Time" onChange={(event) => setTime(event.target.value)} />
 //         <button>Book a table</button>
 //       </form>
+
